b5d41b946d87f5e2775c2069c8d456cacea2b263
lipeng <lipeng@daokoudai.com>
2019-09-06 14:33:40 +0800

    9.NodeJS部署阿里云-实现前后端打包上线v0.4：服务器加载前端静态资源配置

--- uniapp-api/index.js
+++ uniapp-api/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 
 // app.get('/',(req,res)=>{
 //   res.json({msg:'hello aliyun nginx nodejs'});
 // })
+app.use(express.static('client/dist'))
+app.get('/',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'client','dist','index.html'))
+})
 app.get('/api/datas',(req,res)=>{
   res.json({
     name: '米斯特李',
